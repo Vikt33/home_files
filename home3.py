b01006cda4ef265889c6cfc803dbfc68f2898672
Viktor <fomvaf@mail.ru>
2021-09-29 20:05:04 +0300

    Update

--- home3.py
+++ home3.py
@@ -1,63 +1,27 @@
-def rewrite_file(path1=None, path2=None, path3=None):
-    if path1 or path2 or path3 is None:
-        path1 = '1.txt'
-        path2 = '2.txt'
-        path3 = '3.txt'
-        os.chdir('sorted')
-        outout_file = "rewrite_file.txt"
-        file1_path = os.path.join(os.getcwd(), path1)
-        file2_path = os.path.join(os.getcwd(), path2)
-        file3_path = os.path.join(os.getcwd(), path3)
-        with open(file1_path, 'r', encoding='utf-8') as f1:
-            file1 = f1.readlines()
-        with open(file2_path, 'r', encoding='utf-8') as f2:
-            file2 = f2.readlines()
-        with open(file3_path, 'r', encoding='utf-8') as f3:
-            file3 = f3.readlines()
-        with open(outout_file, 'w', encoding='utf-8') as f_total:
+import os
 
-            if len(file1) < len(file2) and len(file1) < len(file3):
-                f_total.write(path1 + '\n')
-                f_total.write(str(len(file1)) + '\n')
-                f_total.writelines(file1)
-                f_total.write('\n')
-            elif len(file2) < len(file1) and len(file2) < len(file3):
-                f_total.write(path2 + '\n')
-                f_total.write(str(len(file2)) + '\n')
-                f_total.writelines(file2)
-                f_total.write('\n')
-            elif len(file3) < len(file1) and len(file3) < len(file2):
-                f_total.write(path3 + '\n')
-                f_total.write(str(len(file3)) + '\n')
-                f_total.writelines(file3)
-                f_total.write('\n')
-            if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(file3):
-                f_total.write(path1 + '\n')
-                f_total.write(str(len(file1)) + '\n')
-                f_total.writelines(file1)
-                f_total.write('\n')
-            elif len(file1) > len(file2) > len(file3) or len(file2) > len(file1) and len(file2) < len(file3):
-                f_total.write(path2 + '\n')
-                f_total.write(str(len(file2)) + '\n')
-                f_total.writelines(file2)
-                f_total.write('\n')
-            elif len(file1) > len(file3) > len(file2) or len(file3) > len(file1) and len(file3) < len(file2):
-                f_total.write(path3 + '\n')
-                f_total.write(str(len(file3)) + '\n')
-                f_total.writelines(file3)
-                f_total.write('\n')
-            if len(file1) > len(file2) and len(file1) > len(file3):
-                f_total.write(path1 + '\n')
-                f_total.write(str(len(file1)) + '\n')
-                f_total.writelines(file1)
-            elif len(file2) > len(file1) and len(file2) > len(file3):
-                f_total.write(path2 + '\n')
-                f_total.write(str(len(file2)) + '\n')
-                f_total.writelines(file2)
-            elif len(file3) > len(file1) and len(file3) > len(file2):
-                f_total.write(path3 + '\n')
-                f_total.write(str(len(file3)) + '\n')
-                f_total.writelines(file3)
-    else:
-        print('Неверный параметр')
-    return
+
+def create_combined_list(directory):
+    file_list = os.listdir(directory)
+    combined_list = []
+
+    for file in file_list:
+        with open(directory + "/" + file, encoding="utf-8") as cur_file:
+            combined_list.append([file, 0, []])
+            for line in cur_file:
+                combined_list[-1][2].append(line.strip())
+                combined_list[-1][1] += 1
+
+    return sorted(combined_list, key=lambda x: x[2], reverse=True)
+
+
+def create_file_from_directory(directory, filename):
+    with open(filename + '.txt', 'w+', encoding="utf-8") as newfile:
+        for file in create_combined_list(directory):
+            newfile.write(f'File name: {file[0]}\n')
+            newfile.write(f'Length: {file[1]} string(s)\n')
+            for string in file[2]:
+                newfile.write(string + '\n')
+            newfile.write('-------------------\n')
+
+create_file_from_directory('test', 'mytext')
